2ac2f8fd163b5bcf60fd89e6da84ce4d7017c6b1
jihoon7712 <jihoon7712@gmail.com>
2024-04-07 07:45:04 +0000

    FIX : MUTE X LOCK

--- Sleep_Detection.py
+++ Sleep_Detection.py
@@ -3,10 +3,11 @@ import signal
 import time
 
 # 부모 프로세스의 PID를 가져옵니다.
-parent_pid = os.getppid()
+
 
 #sleeping Detection 구현
 
 # 시그널을 보냅니다.
+parent_pid = os.getppid()
 print("sleeping parent pid",parent_pid,"\n")
 os.kill(parent_pid, signal.SIGUSR1)

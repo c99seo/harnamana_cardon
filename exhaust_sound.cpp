5c76c57fed193a08b547f0e0cd1d0acb89e885a4
jihoon7712 <jihoon7712@gmail.com>
2024-04-08 06:57:10 +0000

    ADD : pedestrian detection

--- exhaust_sound.cpp
+++ exhaust_sound.cpp
@@ -8,18 +8,23 @@ void pedestrianCheckHandler(int sig){
     //초음파 센서로 거리 측정하고
     //속도에 따라서 제동 거리 계산(90-110db로 제동거리를 scale)
     //보행자 거리를 측정해서 제동 거리 내에 있으면 즉각 가상 배기음 출력
-    printf("Watchout");
+    printf("Watchout\n");
+    sleep(3);
+
 }
 
 int main(){
     pid_t camera = fork();
     if(camera == 0){
+        
         if(execlp("/usr/bin/python3", "python3", "pedestrian_detection.py", NULL) == -1) {
             perror("execlp"); // 오류 메시지 출력
             exit(EXIT_FAILURE); // 실패 시 자식 프로세스 종료
         }
     }
     else{
+        sleep(1);
+        printf("%d\n",getpid());
         struct sigaction pedestriancheck;
         pedestriancheck.sa_handler = pedestrianCheckHandler;
         sigemptyset(&pedestriancheck.sa_mask);
@@ -28,7 +33,6 @@ int main(){
 
         while(true){
             //내부 라즈베리파이로부터 값을 받아오기
-            
         }
     }
 }
